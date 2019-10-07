18effce0a85d1f2bfcb1b6f7c1ece9b03560c505
admin.karima@gmail.com <admin.karima@gmail.com>
2019-10-07 18:17:12 +0300

    rating and game show views tests and ajax implement

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -13,56 +13,59 @@ class GamesController < ApplicationController
 
   def create
     begin
-      # Создаем игру для залогиненного юзера
       @game = Game.create_game_for_user!(current_user)
 
-      # Отправляемся на страницу игры
       redirect_to game_path(@game), notice: I18n.t(
         'controllers.games.game_created',
         created_at: @game.created_at
       )
     rescue ActiveRecord::RecordInvalid, ActiveRecord::RecordNotSaved => ex
-      # Если ошибка создания игры
       Rails.logger.error("Error creating game for user #{current_user.id}, " \
                          "msg = #{ex}. #{ex.backtrace}")
 
-      # Отправляемся назад с алертом
       redirect_to :back, alert: I18n.t('controllers.games.game_not_created')
     end
   end
 
-  # Действие answer принимает ответ на вопрос, единственный обязательный
-  # параметр — params[:letter] — буква, которую выбрал игрок.
   def answer
-    # Выясняем у игры, правильно ли оветили
     @answer_is_correct = @game.answer_current_question!(params[:letter])
     @game_question = @game.current_game_question
 
     unless @answer_is_correct
-      # Если ответили неправильно, отправляем юзера на профиль с сообщением
       flash[:alert] = I18n.t(
-        'controllers.games.bad_answer',
-        answer: @game_question.correct_answer,
-        prize: view_context.number_to_currency(@game.prize)
+          'controllers.games.bad_answer',
+          answer: @game_question.correct_answer,
+          prize: view_context.number_to_currency(@game.prize)
       )
     end
 
-    if @game.finished?
-      # Если игра закончилась, отправялем юзера на свой профиль
-      redirect_to user_path(current_user)
-    else
-      # Иначе, обратно на экран игры
-      redirect_to game_path(@game)
+    respond_to do |format|
+      format.html do
+        if @answer_is_correct && !@game.finished?
+          redirect_to game_path(@game)
+        else
+          redirect_to user_path(current_user)
+        end
+      end
+
+      # Если это js-запрос, то ничего не делаем и контролл попытается отрисовать
+      # шаблон
+      # <controller>/<action>.<format>.erb
+      # В нашем случае будет games/answer.js.erb
+      format.js {}
     end
+
+    # if @game.finished?
+    #   redirect_to user_path(current_user)
+    # else
+    #   # Иначе, обратно на экран игры
+    #   redirect_to game_path(@game)
+    # end
   end
 
-  # Действие take_money вызывается из шаблона, когда пользователь берет кнопку
-  # «Взять деньги». Параметров нет, т.к. вся необходимая информация есть в базе.
   def take_money
-    # Заканчиваем игру
     @game.take_money!
 
-    # Отправялем пользователя на профиль с сообщение о выигрыше
     redirect_to user_path(current_user), flash: {
       warning: I18n.t(
         'controllers.games.game_finished',
@@ -93,7 +96,6 @@ class GamesController < ApplicationController
   end
 
   def goto_game_in_progress!
-    # Вот нам и пригодился наш scope in_progress из модели Game
     game_in_progress = current_user.games.in_progress.first
 
     unless game_in_progress.blank?
@@ -107,7 +109,6 @@ class GamesController < ApplicationController
     @game = current_user.games.find_by(id: params[:id])
 
     if @game.blank?
-      # Если у current_user нет игры - посылаем
       redirect_to root_path, alert: I18n.t(
         'controllers.games.not_your_game'
       )
